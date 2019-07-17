f081eee6256c596dec7a9812fd0c6924b246c40a
Volkan OZSAY <volkanozsay@gmail.com>
2019-07-17 17:01:53 +0300

    Create Models(Role,User,Donation)

--- src/main/java/com/volkanozsay/bagisla/model/User.java
+++ src/main/java/com/volkanozsay/bagisla/model/User.java
@@ -1,16 +1,69 @@
 package com.volkanozsay.bagisla.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity @Table(name = "USER") public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USER_ID")
 	private Long userId;
+	@Column(name = "USER_NAME")
 	private String userName;
+	@Column(name = "PASSWORD")
 	private String password;
+	@Column(name = "USER_INFO")
 	private String userInfo;
-	private Set<Donation> donations;
-	private Role role;
 
+	private Set<Donation> donations;
+	private Set<Role> role;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserInfo() {
+		return userInfo;
+	}
+
+	public void setUserInfo(String userInfo) {
+		this.userInfo = userInfo;
+	}
+
+	public Set<Donation> getDonations() {
+		return donations;
+	}
+
+	public void setDonations(Set<Donation> donations) {
+		this.donations = donations;
+	}
+
+	public Set<Role> getRole() {
+		return role;
+	}
+
+	public void setRole(Set<Role> role) {
+		this.role = role;
+	}
 }
