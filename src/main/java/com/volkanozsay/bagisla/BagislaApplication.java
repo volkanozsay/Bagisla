b61cb01225dd33763563d52af42f4250fd1a7dcf
Volkan OZSAY <volkanozsay@gmail.com>
2019-07-17 16:46:29 +0300

    Create Project Main Structure for Bagisla

---
+++ src/main/java/com/volkanozsay/bagisla/BagislaApplication.java
@@ -0,0 +1,12 @@
+package com.volkanozsay.bagisla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class BagislaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagislaApplication.class, args);
+	}
+
+}
