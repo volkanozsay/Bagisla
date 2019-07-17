93b87bf53f94c1b739c63be52a35d80b86057ac3
Volkan OZSAY <volkanozsay@gmail.com>
2019-07-18 00:20:08 +0300

    Create Repository (User,Donation)

--- src/main/java/com/volkanozsay/bagisla/repository/DonationRepository.java
+++ src/main/java/com/volkanozsay/bagisla/repository/DonationRepository.java
@@ -1,4 +1,14 @@
 package com.volkanozsay.bagisla.repository;
 
-public interface DonationRepository {
+		import com.volkanozsay.bagisla.model.Donation;
+		import org.springframework.data.jpa.repository.JpaRepository;
+		import org.springframework.data.jpa.repository.Query;
+
+		import java.util.List;
+
+public interface DonationRepository extends JpaRepository<Donation, Long> {
+	@Query("SELECT d FROM Donation d") List<Donation> findAll();
+
+	@Query("SELECT d FROM Donation d where d.donationId=?1") Donation findByDonationId(Long id);
+
 }
